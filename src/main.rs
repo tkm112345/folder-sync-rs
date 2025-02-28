272aa59b416a91df238fd72efd1189c3d08b19be
myotherpc <myother@myother.com>
2025-02-28 12:24:40 +0900

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-// cspell:ignore simplelog PKGNAME
+// cspell:ignore simplelog PKGNAME indifatif
 use std::io::{self};
+use indicatif::{ProgressBar,ProgressStyle};
 use clap::{Parser,Subcommand};
 use serde::Deserialize;
 use std::{io::Read,io::Write,fs,thread};
 use std::path::Path;
-use log::{error,info, LevelFilter};
-// use simplelog::*;
+use log::{error,info};
 use log4rs;
-use std::sync::Arc;
+use std::sync::{Arc,mpsc};
 
 const VERSION :&str = env!("CARGO_PKG_VERSION");
 const PKGNAME: &str = env!("CARGO_PKG_NAME");
@@ -64,12 +64,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         fs::create_dir_all("./log")?;
     }
 
-    // ログファイルを開く(追記モード)
-    let file = fs::OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("./log/main.log")?;
-
     // ログの設定
     log4rs::init_file("log4rs.yaml",Default::default())?;
     
@@ -85,12 +79,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 info!("Backup mode");
             }
             let mut handles = vec![];
+            let total_files = count_files(&config.bts)?;
+            let progress_bar = Arc::new(ProgressBar::new(total_files));
+            progress_bar.set_style(
+                ProgressStyle::default_bar()
+                    .template("{msg} [{bar:40.cyan/blue}] {pos}/{len} ({percent}%)")?
+                    .progress_chars("#>-"),
+            );
+            progress_bar.set_message("Backing up");
+
             for bts_config in &config.bts {
                 let bts_config = Arc::new(bts_config.clone());
+                let progress_bar = Arc::clone(&progress_bar);
                 let handle = thread::spawn({
                     let bts_config = Arc::clone(&bts_config);
                     move || {
-                        if let Err(err) = backup_to_ssd(&bts_config){
+                        if let Err(err) = backup_to_ssd(&bts_config, &progress_bar){
                             error!("Backup failed : {}",err);
                         }
                     }
@@ -100,6 +104,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             for handle in handles {
                 handle.join().unwrap();
             }
+            progress_bar.finish_with_message("Backup complete");
         }
         Commands::CreateFolders => {
             if cli.verbose {
@@ -130,7 +135,50 @@ fn load_config(path : &str) -> Result<AppConfig, Box<dyn std::error::Error>> {
     Ok(config)
 }
 
-fn backup_to_ssd(config : &BtsConfig) -> Result<(),Box<dyn std::error::Error>> {
+fn count_files(bts_configs: &[BtsConfig]) -> Result<u64, Box<dyn std::error::Error>> {
+    let (tx,rx) = mpsc::channel();
+    let mut handles = vec![];
+
+    for config in bts_configs {
+        let tx = tx.clone();
+        let source_path = Path::new(&config.source).to_path_buf();
+        let handle = thread::spawn(move || {
+            let count = count_files_recursive(&source_path).unwrap_or(0);
+            tx.send(count).unwrap();
+        });
+        handles.push(handle);
+    }
+
+    drop(tx); // メインスレッドの送信側をドロップして、終了を通知
+    
+    let mut total_files = 0;
+    for count in rx {
+        total_files += count;
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    Ok(total_files)
+
+}
+
+fn count_files_recursive(path: &Path) -> Result<u64, Box<dyn std::error::Error>> {
+    let mut count = 0;
+    if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let path = entry.path();
+            count += count_files_recursive(&path)?;
+        }
+    } else {
+        count += 1;
+    }
+    Ok(count)
+}
+
+fn backup_to_ssd(config : &BtsConfig,progress_bar : &Arc<ProgressBar>) -> Result<(),Box<dyn std::error::Error>> {
     // println!("Source : {}, Destination : {}",config.bgs.source, config.bgs.destination);
     // println!("Overwrite : {}, Exclude : {:?}",config.bgs.overwrite, config.bgs.exclude);
 
@@ -143,7 +191,7 @@ fn backup_to_ssd(config : &BtsConfig) -> Result<(),Box<dyn std::error::Error>> {
     }
 
     // ディレクトリを再帰的にコピー
-    copy_recursive(source_path,destination_path,config.overwrite, &config.exclude)?;
+    copy_recursive(source_path,destination_path,config.overwrite, &config.exclude,progress_bar)?;
 
     Ok(())
 }
@@ -153,6 +201,7 @@ fn copy_recursive(
     destination : &Path,
     overwrite : bool,
     exclude : &[String],
+    progress_bar : &Arc<ProgressBar>
 ) -> Result<(),Box<dyn std::error::Error>> {
     // 除外リストに含まれる場合はスキップ
     if exclude.iter().any(|e| source.ends_with(e)){
@@ -169,7 +218,7 @@ fn copy_recursive(
             let entry = entry?;
             let path = entry.path();
             let destination = destination.join(entry.file_name());
-            copy_recursive(&path, &destination, overwrite, exclude)?;
+            copy_recursive(&path, &destination, overwrite, exclude,progress_bar)?;
         }
     } else {
         // ファイルをコピー
@@ -195,6 +244,7 @@ fn copy_recursive(
 
         fs::copy(source,destination)?;
         info!("Copied: {} to {}", source.display(), destination.display());
+        progress_bar.inc(1);
     }
 
     Ok(())
